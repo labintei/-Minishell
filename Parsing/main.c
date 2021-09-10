5257eb31b2bad7fc9397207d13ce5a4cc5da5bad
Lauranne BINTEIN <labintei@e2r6p19.clusters.42paris.fr>
2021-09-10 18:04:39 +0200

    fix Leaks Parsing

--- Parsing/main.c
+++ Parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: malatini <malatini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 18:48:50 by labintei          #+#    #+#             */
-/*   Updated: 2021/09/10 17:20:47 by labintei         ###   ########.fr       */
+/*   Updated: 2021/09/10 18:04:18 by labintei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	add_list_file(t_list_file **file, int i, char	c)
 	}
 }
 
-int		find_var_and_strlen(char *line, int *j, t_env *env)
+int	find_var_and_strlen(char *line, int *j, t_env *env)
 {
 	char	*new;
 	int		a;
@@ -52,7 +52,7 @@ int		find_var_and_strlen(char *line, int *j, t_env *env)
 	while (line && line[(*j) + a] && is_alphanum(line[(*j) + a]))
 		a++;
 	new = malloc(sizeof(char) * (a + 1));
-	a  = 0;
+	a = 0;
 	while (line && line[(*j)] && is_alphanum(line[(*j)]))
 	{
 		new[a] = line[(*j)];
@@ -66,46 +66,52 @@ int		find_var_and_strlen(char *line, int *j, t_env *env)
 	return (ret);
 }
 
-int		nb_redir(char *line, int i)
+void	skip_quotes(char *line, int *j)
 {
-	int		j;
 	char	c;
-	int		redir;
-	
-	redir = 0;
-	j = i;
-	while (line && line[j])
+
+	if (line && line[(*j)] && is_quotes(line[(*j)], line, (*j)))
 	{
-		if ((line[j] == '\'' || line[j] == '\"') && ft_second(line[j], line, j))
+		c = line[(*j)];
+		(*j)++;
+		while (line[(*j)] && line[(*j)] != c)
+			(*j)++;
+		(*j)++;
+	}
+}
+
+int	nb_redir(char *line, int h)
+{
+	int		i[3];
+
+	i[1] = 0;
+	i[0] = h;
+	while (line && line[i[0]])
+	{
+		if (line[i[0]] && is_quotes(line[i[0]], line, i[0]))
+			skip_quotes(line, &(i[0]));
+		else if (line[i[0]] && line[i[0]] == '|')
 		{
-			c = line[j];
-			j++;
-			while (line[j] && line[j] != c)
-				j++;
-			j++;
+			return (i[1]);
 		}
-		else if (line[j] && line[j] == '|')
+		else if (line[i[0]] && ((line[i[0]] == '<') | (line[i[0]] == '>')))
 		{
-			return (redir);
-		}
-		else if(line[j] && ((line[j] == '<') | (line[j] == '>')))
-		{
-			c = line[j];
-			j++;
-			redir++;
-			if (line[j] && line[j] == c)
-				j++;
+			i[2] = line[i[0]];
+			(i[0])++;
+			(i[1])++;
+			if (line[i[0]] && line[i[0]] == (char)i[2])
+				(i[0])++;
 		}
 		else
-			j++;
+			(i[0])++;
 	}
-	return(redir);
+	return (i[1]);
 }
 
 void	ajout_cmds(t_env *env, char *line, int *i)
 {
 	add_cmds(&(env->cmds));
-	if(!(env->cmds->file))
+	if (!(env->cmds->file))
 		return ;
 	env->cmds->cmds = NULL;
 	env->cmds->file = NULL;
@@ -113,50 +119,50 @@ void	ajout_cmds(t_env *env, char *line, int *i)
 	(count_word(line, i) + 1 - nb_redir(line, (*i))));
 }
 
-void	is_pipe(t_env *env, int *i, int *word, char *line, int		*is_cmds)
+void	is_pipe(t_env *env, char *line)
 {
 	env->cmds->type = '|';
-	if(*is_cmds)
-		env->cmds->cmds[(*word)] = NULL;
-	(*is_cmds) = 1;
-	(*i)++;
-	(*word) = 0;
-	ajout_cmds(env, line, i);
+	if (env->is_cmds)
+		env->cmds->cmds[env->word] = NULL;
+	env->is_cmds = 1;
+	(env->i)++;
+	env->word = 0;
+	ajout_cmds(env, line, &(env->i));
 }
 
-int		is_redir(char *line, int *i, t_env *env)
+int	is_redir(char *line, int *i, t_env *env)
 {
 	char	c;
-	
+
 	c = line[(*i)];
 	(*i)++;
-	if(line[(*i)] && line[(*i) - 1] && line[(*i)] == line[((*i) - 1)])
+	if (line[(*i)] && line[(*i) - 1] && line[(*i)] == line[((*i) - 1)])
 	{
 		c = 'L';
-		if(line[(*i)] == '>')
+		if (line[(*i)] == '>')
 			c = 'R';
 		(*i)++;
 	}
 	skip_space(line, i);
-	if(line[(*i)] && (line[(*i)] == '<' || line[(*i)] == '>'))
-		return(2);
+	if (line[(*i)] && (line[(*i)] == '<' || line[(*i)] == '>'))
+		return (2);
 	add_list_file(&(env->cmds->file), count_char(line, (*i), env), c);
-	return(0);
+	return (0);
 }
 
-void		is_quotes_cmds(char *line, int *count,  t_env *env)
+void	is_quotes_cmds(char *line, int *count, t_env *env)
 {
 	char	c;
 
 	c = line[(env->i)];
 	(env->i)++;
-	while(line[(env->i)] && line[(env->i)] != c)
+	while (line[(env->i)] && line[(env->i)] != c)
 	{
-		if(c == '\"' && line[(env->i)] == '$')
+		if (c == '\"' && line[(env->i)] == '$')
 			find_var_and_strlen_cmds(line, env, count);
 		else
 		{
-			if(env->is_cmds)
+			if (env->is_cmds)
 				env->cmds->cmds[(env->word)][(*count)] = line[(env->i)];
 			else
 				env->cmds->file->path[(*count)] = line[(env->i)];
@@ -167,19 +173,19 @@ void		is_quotes_cmds(char *line, int *count,  t_env *env)
 	(env->i)++;
 }
 
-void	ft_dup_env(t_list_env	**env, char *var, char **s)
+void	ft_dup_env(t_list_env **env, char *var, char **s)
 {
-	int		a;
+	int			a;
 	t_list_env	*l;
 
 	l = (*env);
-	while(l)
+	while (l)
 	{
-		if(l->var && ft_strcmp(l->var, var))
+		if (l->var && ft_strcmp(l->var, var))
 		{
 			(*s) = malloc(sizeof(char) * (ft_strlen(l->val) + 1));
-			a  = -1;
-			while(l->val && l->val[(++a)])
+			a = -1;
+			while (l->val && l->val[(++a)])
 				(*s)[a] = l->val[a];
 			(*s)[a] = '\0';
 			return ;
@@ -188,21 +194,21 @@ void	ft_dup_env(t_list_env	**env, char *var, char **s)
 	}
 }
 
-int			find_var_and_strlen_cmds(char *line, t_env *env, int *count)
+int	find_var_and_strlen_cmds(char *line, t_env *env, int *count)
 {
 	char	*new;
 	char	*stock;
-	int	a;
-	int	ret;
+	int		a;
+	int		ret;
 
 	a = 0;
 	ret = 0;
 	(env->i)++;
-	while(line && line[(env->i) + a] && is_alphanum(line[(env->i) + a]))
+	while (line && line[(env->i) + a] && is_alphanum(line[(env->i) + a]))
 		a++;
 	new = malloc(sizeof(char) * (a + 1));
-	a  = 0;
-	while(line && line[(env->i)] && is_alphanum(line[(env->i)]))
+	a = 0;
+	while (line && line[(env->i)] && is_alphanum(line[(env->i)]))
 	{
 		new[a] = line[(env->i)];
 		a++;
@@ -212,45 +218,45 @@ int			find_var_and_strlen_cmds(char *line, t_env *env, int *count)
 	stock = NULL;
 	ft_dup_env(&(env->env), new, &stock);
 	a = 0;
-	while(stock && stock[a])
+	while (stock && stock[a])
 	{
-		if(env->is_cmds)
+		if (env->is_cmds)
 			env->cmds->cmds[(env->word)][(*count)] = stock[a];
 		else
 			env->cmds->file->path[(*count)] = stock[a];
 		(*count)++;
 		a++;
 	}
-	if(new)
+	if (new)
 		free(new);
-	if(stock)
+	if (stock)
 		free(stock);
-	return(ret);
+	return (ret);
 }
 
-int		is_only_space(char *line)
+int	is_only_space(char *line)
 {
-	int  i;
+	int	i;
 
 	i = 0;
-	while(line && line[i] == ' ')
+	while (line && line[i] == ' ')
 		i++;
-	if(line[i] == '\0')
-		return(1);
-	return(0);
+	if (line[i] == '\0')
+		return (1);
+	return (0);
 }
 
 void	not_quotes_cmds(char *line, t_env *env, int *count, int *out)
 {
-	while(line[(env->i)] && (!(is_quotes(line[(env->i)], line, (env->i)))) && \
+	while (line[(env->i)] && (!(is_quotes(line[(env->i)], line, (env->i)))) && \
 	line[(env->i)] != '|' && line[(env->i)] != ' ' && line[(env->i)] != '<' && \
 	line[(env->i)] != '>')
 	{
-		if(line[(env->i)] && line[(env->i)] == '$')
+		if (line[(env->i)] && line[(env->i)] == '$')
 			find_var_and_strlen_cmds(line, env, count);
 		else
 		{
-			if(env->is_cmds == 1)
+			if (env->is_cmds == 1)
 				env->cmds->cmds[(env->word)][(*count)] = line[(env->i)];
 			else
 				env->cmds->file->path[(*count)] = line[(env->i)];
@@ -258,47 +264,48 @@ void	not_quotes_cmds(char *line, t_env *env, int *count, int *out)
 			env->i++;
 		}
 	}
-	if(line[(env->i)] && line[(env->i)] == '|')
+	if (line[(env->i)] && line[(env->i)] == '|')
 		*out = 1;
-	if(line[(env->i)] && (line[(env->i)] == '<' || line[(env->i)] == '>'))
+	if (line[(env->i)] && (line[(env->i)] == '<' || line[(env->i)] == '>'))
 		*out = 1;
-
 }
 
-void		is_word_cmds(char *line, int *i, t_env *env)
+void	is_word_cmds(char *line, int *i, t_env *env)
 {
 	int		count;
 	int		out;
 
-	out  = 0;
+	out = 0;
 	count = 0;
-	if((env->is_cmds))
+	if ((env->is_cmds))
 	{
-		if(!env->cmds->cmds)
-			env->cmds->cmds = malloc(sizeof(char *) * (count_word(line, i) - nb_redir(line, (*i)) + 1));
-		env->cmds->cmds[(env->word)] = malloc(sizeof(char) * (count_char(line, (*i), env) + 1));
+		if (!env->cmds->cmds)
+			env->cmds->cmds = malloc(sizeof(char *) * \
+			(count_word(line, i) - nb_redir(line, (*i)) + 1));
+		env->cmds->cmds[(env->word)] = malloc(sizeof(char) * \
+		(count_char(line, (*i), env) + 1));
 	}
-	if(line[(*i)] && line[(*i)] != '|' && line[(*i)] != ' ')
+	if (line[(*i)] && line[(*i)] != '|' && line[(*i)] != ' ')
 	{
-		while(line[(*i)] && line[(*i)] != ' ' && out == 0)
+		while (line[(*i)] && line[(*i)] != ' ' && out == 0)
 		{
-			if(is_quotes(line[(*i)], line, (*i)))
+			if (is_quotes(line[(*i)], line, (*i)))
 				is_quotes_cmds(line, &count, env);
 			else
 				not_quotes_cmds(line, env, &count, &out);
 		}
 	}
-	if((env->is_cmds))
+	if ((env->is_cmds))
 		env->cmds->cmds[(env->word)][count] = '\0';
 	else
 		env->cmds->file->path[count] = '\0';
-	if((env->is_cmds))
+	if ((env->is_cmds))
 		(env->word)++;
-	if((env->is_cmds) == 0)
+	if ((env->is_cmds) == 0)
 		(env->is_cmds) = 1;
 }
 
-int		count_redir(char *line, int j)
+int	count_redir(char *line, int j)
 {
 	int		i;
 	int		redir;
@@ -308,28 +315,28 @@ int		count_redir(char *line, int j)
 	redir = 0;
 	i = j;
 	stop = 0;
-	while(line[i] && stop == 0)
+	while (line[i] && stop == 0)
 	{
-		if(line[i] && stop == 0 && (line[i] == '\'' || line[i] == '\"') && ft_second(line[i], line, i))
+		if (line[i] && stop == 0 && (line[i] == '\'' || line[i] == '\"') && ft_second(line[i], line, i))
 		{
 			c = line[i];
 			i++;
-			while(line[i] && line[i] != c)
+			while (line[i] && line[i] != c)
 				i++;
-			if(line[i] == c)
+			if (line[i] == c)
 				i++;
 		}
 		else
 		{
-			while(line[i] && stop == 0 && !((line[i] == '\'' || line[i] == '\"') && ft_second(line[i], line, i)))
+			while (line[i] && stop == 0 && !((line[i] == '\'' || line[i] == '\"') && ft_second(line[i], line, i)))
 			{
-				if(line[i] && line[i] == '|')
+				if (line[i] && line[i] == '|')
 					stop = 1;
-				else if(line[i] && (line[i] == '<' || line[i] == '>'))
+				else if (line[i] && (line[i] == '<' || line[i] == '>'))
 				{
 					c = line[i];
 					i++;
-					if(line[i] == c)
+					if (line[i] == c)
 						i++;
 					redir++;
 				}
@@ -337,10 +344,10 @@ int		count_redir(char *line, int j)
 			}
 		}
 	}
-	return(redir);
+	return (redir);
 }
 
-int			count_word_before_redir(char	*line, int  j)
+int	count_word_before_redir(char *line, int  j)
 {
 	int			word;
 	int			i;
@@ -352,38 +359,40 @@ int			count_word_before_redir(char	*line, int  j)
 	stop = 0;
 	i = j;
 	word = 0;
-	while(line[i] && stop == 0)
+	while (line[i] && stop == 0)
 	{
-		if(line[i] && line[i] == '|')
+		if (line[i] && line[i] == '|')
 			stop = 1;
-		else if(line[i] && line[i] == ' ' && stop == 0)
+		else if (line[i] && line[i] == ' ' && stop == 0)
 		{
-			while(line[i] && line[i] == ' ')
+			while (line[i] && line[i] == ' ')
 				i++;
 		}
-		else if(line[i] && stop == 0)
+		else if (line[i] && stop == 0)
 		{
-			if(line[i] && (line[i] != '<' && line[i] != '>'))
+			if (line[i] && (line[i] != '<' && line[i] != '>'))
 				word++;
 			stop_bis = 0;
-			while(line[i] && stop_bis == 0)
+			while (line[i] && stop_bis == 0)
 			{
-				if(line[i] && ((line[i] == '\'' || line[i] == '\"') && ft_second(line[i], line, i)))
+				if (line[i] && ((line[i] == '\'' || line[i] == '\"') && \
+				ft_second(line[i], line, i)))
 				{
 					c = line[i];
 					i++;
-					while(line[i] && line[i] != c)
+					while (line[i] && line[i] != c)
 						i++;
-					if(line[i] && line[i] == c)
+					if (line[i] && line[i] == c)
 						i++;
 				}
 				else
 				{
-					while(line[i] && (!((line[i] == '\'' || line[i] == '\"') && ft_second(line[i], line, i))) && stop_bis == 0)
+					while (line[i] && (!((line[i] == '\'' || line[i] == '\"') \
+					&& ft_second(line[i], line, i))) && stop_bis == 0)
 					{
-						if(line[i] && line[i] == ' ')
+						if (line[i] && line[i] == ' ')
 							stop_bis = 1;
-						else if(line[i] == '>' || line[i] == '<')
+						else if (line[i] == '>' || line[i] == '<')
 						{
 							return(word);
 						}
@@ -393,12 +402,12 @@ int			count_word_before_redir(char	*line, int  j)
 			}
 		}
 	}
-	return(word);
+	return (word);
 }
 
-void	parse_line(t_env *env, char *line)
+void	parse_line(t_env *env, char *l)
 {
-	if(!line || line[0] == '\0' || is_only_space(line))
+	if (!l || l[0] == '\0' || is_only_space(l))
 	{
 		env->cmds = NULL;
 		return ;
@@ -407,28 +416,28 @@ void	parse_line(t_env *env, char *line)
 	env->i = 0;
 	env->word = 0;
 	env->is_cmds = 1;
-	ajout_cmds(env, line, &(env->i));
-	while(line && line[(env->i)])
+	ajout_cmds(env, l, &(env->i));
+	while (l && l[(env->i)])
 	{
-		skip_space(line, &(env->i));
-		if(line[(env->i)] && line[(env->i)] == '|')
-			is_pipe(env, &(env->i), &(env->word), line, &(env->is_cmds));
-		else if(line[(env->i)] && line[(env->i)] != '|' && line[(env->i)] != ' ')
+		skip_space(l, &(env->i));
+		if (l[(env->i)] && l[(env->i)] == '|')
+			is_pipe(env, l);
+		else if (l[(env->i)] && l[(env->i)] != '|' && l[(env->i)] != ' ')
 		{
-			if(line[(env->i)] && (line[(env->i)] == '>' || line[(env->i)] == '<'))
+			if (l[(env->i)] && (l[(env->i)] == '>' || l[(env->i)] == '<'))
 			{
-				is_redir(line, &(env->i), env);
+				is_redir(l, &(env->i), env);
 				env->is_cmds = 0;
 			}
 			else
-				is_word_cmds(line, &(env->i), env);
+				is_word_cmds(l, &(env->i), env);
 		}
 	}
-	if(env->is_cmds && env->cmds && env->cmds->cmds)
+	if (env->is_cmds && env->cmds && env->cmds->cmds)
 		env->cmds->cmds[(env->word)] = NULL;
 }
 
-int		start_parse(t_env	*env)
+int	start_parse(t_env *env)
 {
 	char	*line;
 
@@ -438,7 +447,7 @@ int		start_parse(t_env	*env)
 		if (line)
 		{
 			parse_line(env, line);
-			if(env->cmds)
+			if (env->cmds)
 			{
 				exec_cmds(env);
 				clear_cmds(&(env->cmds));
@@ -463,7 +472,7 @@ void	init_env(t_env *env)
 	env->split_path = NULL;
 }
 
-int		main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	t_env	env;
 	int		ret;
